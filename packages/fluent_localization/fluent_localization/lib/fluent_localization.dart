3df119eab6c7b4d1f4ba5402830af382cb6041fb
Alfonso Israel Osorio Avilez <aosorio.avilez@gmail.com>
2023-04-15 12:24:41 -0600

    Export apis via implementations

--- packages/fluent_localization/fluent_localization/lib/fluent_localization.dart
+++ packages/fluent_localization/fluent_localization/lib/fluent_localization.dart
@@ -1,4 +1,6 @@
 /// Library that allows you to translate your app texts
 library fluent_localization;
 
+export 'package:fluent_localization_api/fluent_localization_api.dart'
+    show LocalizationApi;
 export 'src/localization_module.dart';
--- packages/fluent_networking/fluent_networking/lib/fluent_networking.dart
+++ packages/fluent_networking/fluent_networking/lib/fluent_networking.dart
@@ -1,6 +1,8 @@
 /// Package that provides a simple way to make http requests
 library fluent_networking;
 
+export 'package:fluent_networking_api/fluent_networking_api.dart'
+    show NetworkingApi;
 export 'src/http_method.dart';
 export 'src/networking_module.dart';
 export 'src/rest_api_interceptor.dart';
