1b213e225557736cb93ce03544a6cda38f527410
Alfonso Israel Osorio Avilez <aosorio.avilez@gmail.com>
2023-04-18 13:10:50 -0600

    Expose RestApiResult

--- packages/fluent_networking/fluent_networking/lib/fluent_networking.dart
+++ packages/fluent_networking/fluent_networking/lib/fluent_networking.dart
@@ -2,7 +2,7 @@
 library fluent_networking;
 
 export 'package:fluent_networking_api/fluent_networking_api.dart'
-    show NetworkingApi;
+    show NetworkingApi, RestApiResult;
 export 'src/http_method.dart';
 export 'src/networking_module.dart';
 export 'src/rest_api_interceptor.dart';
