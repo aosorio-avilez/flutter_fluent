71fdfa187e69cb9c2f7a615d1d1934397d69bca2
Alfonso Israel Osorio Avilez <aosorio.avilez@gmail.com>
2023-04-26 12:00:28 -0600

    Removed library keyword from fluent_sdk

--- packages/fluent_sdk/lib/fluent_sdk.dart
+++ packages/fluent_sdk/lib/fluent_sdk.dart
@@ -1,9 +1,4 @@
-/// Code base to modularize features through a service locator.
-library fluent_sdk;
-
-// export 'src/api/environment.dart';
 export 'src/api/fluent_module.dart';
 export 'src/api/registry.dart';
-// export 'src/environment_type.dart';
 export 'src/fluent.dart';
 export 'src/fluent_route.dart';
