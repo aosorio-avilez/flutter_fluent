c04eeeb8bd0e0d84465b31699f3db748f1172a09
Alfonso Israel Osorio Avilez <aosorio.avilez@gmail.com>
2023-07-05 12:37:25 -0600

    feat(fluent_navigation): Nested navigation support added (#35)
    * Import only route file from go router
    
    * Removed debugLabel

--- packages/fluent_navigation/fluent_navigation/lib/fluent_navigation.dart
+++ packages/fluent_navigation/fluent_navigation/lib/fluent_navigation.dart
@@ -1,6 +1,7 @@
 export 'package:fluent_navigation_api/fluent_navigation_api.dart';
 export 'package:fluent_sdk/fluent_sdk.dart';
-export 'package:go_router/go_router.dart';
+export 'package:go_router/src/pages/custom_transition_page.dart';
+export 'package:go_router/src/route.dart';
 
 export 'src/navigation_module.dart';
 export 'src/registry_extension.dart';
