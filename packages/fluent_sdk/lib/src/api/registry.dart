cc76ea9a08000a0084cc0e44807c7a7c6a1d5060
Alfonso Israel Osorio Avilez <aosorio.avilez@gmail.com>
2024-05-18 11:02:28 -0600

    feat(fluent_sdk): Improve Fluent SDK (#45)
    * Upgrade dependencies
    
    * Add better documentation and improve build method

--- packages/fluent_sdk/lib/src/api/registry.dart
+++ packages/fluent_sdk/lib/src/api/registry.dart
@@ -1,29 +1,89 @@
 import 'package:get_it/get_it.dart';
 
-/// Interface defined to register module dependencies
 abstract class Registry {
-  /// Enable/Disable the ability to register a new object
-  /// although the object is already registered
+  /// Allows or disallows the reassignment of a singleton object.
+  ///
+  /// This method is useful for testing purposes, as it allows you to
+  /// register a mock implementation of a registered object.
+  ///
+  /// It takes a single argument, [allow], which is a boolean that
+  /// indicates whether the reassignment of a singleton object is allowed.
+  ///
+  /// If [allow] is true, it allows the reassignment of a singleton
+  /// object. If [allow] is false, it disallows the reassignment of a
+  /// singleton object.
+  ///
+  /// By default, the reassignment of a singleton object is disallowed.
   void allowReassignment({required bool allow});
 
-  /// Register a new lazy singleton object
+  /// Registers a new lazy singleton object
+  ///
+  /// This method takes a single argument, [factoryFunction], which is a
+  /// function that returns an instance of the type [T].
+  ///
+  /// It also takes an optional argument, [instanceName], which is the name
+  /// of the instance to be registered.
+  ///
+  /// [factoryFunction] is a function that returns an instance of the
+  /// type [T].
+  ///
+  /// [instanceName] is the name of the instance to be registered.
   void registerLazySingleton<T extends Object>(
     T Function(GetIt) factoryFunction, {
     String? instanceName,
   });
 
-  /// Register a new singleton object
+  /// Registers a new singleton object
+  ///
+  /// This method takes a single argument, [factoryFunction], which is a
+  /// function that returns an instance of the type [T].
+  ///
+  /// It also takes an optional argument, [instanceName], which is the name
+  /// of the instance to be registered.
+  ///
+  /// [factoryFunction] is a function that returns an instance of the
+  /// type [T].
+  ///
+  /// [instanceName] is the name of the instance to be registered.
+  ///
+  /// If [instanceName] is not provided, the instance will be registered
+  /// with the default name, which is the type of [T].
   void registerSingleton<T extends Object>(
     T Function(GetIt) factoryFunction, {
     String? instanceName,
   });
 
-  /// Register a new factory object
+  /// Registers a new factory object
+  ///
+  /// This method takes a single argument, [factoryFunction], which is a
+  /// function that returns an instance of the type [T].
+  ///
+  /// It also takes an optional argument, [instanceName], which is the name
+  /// of the instance to be registered.
+  ///
+  /// [factoryFunction] is a function that returns an instance of the
+  /// type [T].
+  ///
+  /// [instanceName] is the name of the instance to be registered.
+  ///
+  /// If [instanceName] is not provided, the instance will be registered
+  /// with the default name, which is the type of [T].
+  ///
+  /// This method is useful when you want to register a factory that will
+  /// be used to create objects of type [T].
   void registerFactory<T extends Object>(
     T Function(GetIt) factoryFunction, {
     String? instanceName,
   });
 
-  /// Return if an object is already registered or not
-  bool isRegistered<T extends Object>();
+  /// Checks if an instance of type [T] is registered.
+  ///
+  /// It returns true if an instance of type [T] is registered, and false
+  /// otherwise.
+  ///
+  /// [T] is the type of the instance to be checked.
+  bool isRegistered<T extends Object>() {
+    /// Checks if an instance of type [T] is registered.
+    return GetIt.instance.isRegistered<T>();
+  }
 }
