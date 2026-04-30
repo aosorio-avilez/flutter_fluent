713fa9d8aaac8a21ea6bf29ab1591adab3a29d1c
Alfonso Israel Osorio Avilez <aosorio.avilez@gmail.com>
2026-04-30 13:10:25 -0600

    fix(navigation): suppress linter warnings for pub.dev compatibility

--- packages/fluent_navigation/fluent_navigation/lib/src/navigation_module.dart
+++ packages/fluent_navigation/fluent_navigation/lib/src/navigation_module.dart
@@ -49,6 +49,7 @@ class NavigationModule extends FluentModule {
       ..registerLazySingleton<InternalNavigationApi>(
         (it) {
           if (!it.isRegistered<FluentRoutes>()) {
+            // ignore: avoid_types_on_closure_parameters, Required for pub.dev analysis compatibility during downgrade tests.
             it.registerSingleton<FluentRoutes>((Registry it) => <RouteBase>[]);
           }
           return InternalNavigationApiImpl(it<FluentRoutes>());
